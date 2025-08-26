cd81db11685fa79aba2abf6dabfcb31e3b69b9cf
박준규 <junju404@naver.com>
2025-08-27 08:39:49 +0900

    refactor: 컴포넌트 변수화

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import DashboardView from '../views/DashboardView.vue'
+import EquipmentListView from '../views/EquipmentListView.vue'
+import EquipmentDetailView from '../views/EquipmentDetailView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,18 +11,16 @@ const router = createRouter({
       name: 'dashboard',
       component: DashboardView,
     },
-    // 물류 장비별 라우트
-    // 동적 장비 라우트
     {
       path: '/equipment/:type',
       name: 'equipment-list',
-      component: () => import('../views/EquipmentListView.vue'),
+      component: EquipmentListView,
       props: true,
     },
     {
       path: '/equipment/:type/:id',
       name: 'equipment-detail',
-      component: () => import('../views/EquipmentDetailView.vue'),
+      component: EquipmentDetailView,
       props: true,
     },
   ],
