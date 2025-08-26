278b32f4f2bc616ad728f426f379900e6b21f53b
박준규 <junju404@naver.com>
2025-08-27 08:43:20 +0900

    fix: 화면 크기에 맞추도록 수정

--- src/App.vue
+++ src/App.vue
@@ -30,9 +30,27 @@ body {
 }
 
 #app {
-  width: 100vw;
-  height: 100vh;
-  margin: 0;
-  padding: 0;
+  width: 100vw !important;
+  height: 100vh !important;
+  margin: 0 !important;
+  padding: 0 !important;
+  max-width: none !important;
+  display: block !important;
+  grid-template-columns: none !important;
+}
+
+@media (min-width: 1024px) {
+  body {
+    display: block !important;
+    place-items: initial !important;
+  }
+
+  #app {
+    display: block !important;
+    grid-template-columns: none !important;
+    padding: 0 !important;
+    width: 100vw !important;
+    height: 100vh !important;
+  }
 }
 </style>
